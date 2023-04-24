e737b67b51e0ef756283ede5a2055c06bc448080
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-04-24 09:40:48 +0100

    chore: use 'transform' for the ts-jest config
    They've deprecated the 'globals' way of setting options.
    
    Refs https://github.com/kulshekhar/ts-jest/pull/3780

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-      isolatedModules: true,
-    },
-  },
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['./src/', './test/'],
   moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        isolatedModules: true,
+      },
+    ],
+  },
 }
